387f323da53bb85d0e53a20ae41f0c0a8be33a11
Juhee <juhee01176@gmail.com>
2025-05-12 12:23:26 +0900

    소켓 추가

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,77 +4,136 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView webView;
+    EditText editText;
 
+    TextView sendText;
+    TextView serverText;
+
+    Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_webview);
-//
-//        webView = findViewById(R.id.webView);
-//
-//        webView.setWebViewClient(new WebViewClient());
-//        webView.setWebChromeClient(new WebChromeClient());
-//
-//        webView.getSettings().setLoadWithOverviewMode(true);
-//        webView.getSettings().setUseWideViewPort(true);
-//
-//        webView.loadUrl("http://www.naver.com");
-
         setContentView(R.layout.activity_main);
 
-        Button btnSocket = findViewById(R.id.btnSocket);
-        btnSocket.setOnClickListener(v -> connectSocket());
+        editText = findViewById(R.id.editText);
+        sendText = findViewById(R.id.sendText);
+        serverText = findViewById(R.id.serverText);
+
+        Button sendButton = findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(v -> {
+            final String data = editText.getText().toString();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    send(data);
+                }
+            }).start();
+        });
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(v -> {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    startServer();
+                }
+            }).start();
+        });
     }
 
-    private void connectSocket() {
-        Log.d("connectSocket", "소켓 연결 클릭");
-        new Thread(() -> {
-            try {
-                Log.d("connectSocket", "소켓 연결 스레드");
-                Socket socket = new Socket("10.0.0.199",12456);
-                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    public void send(String data) {
+        try {
+            int portNumber = 5001;
+            Socket socket = new Socket("localhost", portNumber);
+            printClientLog("소켓 연결됨");
+            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+            outputStream.writeObject(data);
+            outputStream.flush();
+            printClientLog("데이터 전송함");
 
-                output.println("Hello Server");
-                String response = input.readLine();
+            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+            printClientLog("서버로부터 받음: " + inputStream.readObject());
+            socket.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-                runOnUiThread(() -> showNotification(this, response));
+    public void startServer() {
+        try {
+            int portNumber = 5001;
+
+            ServerSocket server = new ServerSocket(portNumber);
+            printServerLog("서버 시작함: " +portNumber);
+
+            while (true) {
+                Socket socket = server.accept();
+                InetAddress clientHost = socket.getLocalAddress();
+                int clientPort = socket.getPort();
+                printServerLog("클라이언트 연결됨: " + clientHost + ":" + clientPort);
+
+                ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+                Object obj = inputStream.readObject();
+                printServerLog("데이터 받음: " + obj);
+
+                ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+                outputStream.writeObject(obj + " from server");
+                outputStream.flush();
+                printServerLog("데이터 보냄");
 
                 socket.close();
             }
-            catch (Exception e) {
-                e.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printClientLog(String data) {
+        Log.d("MainActivity", data);
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                sendText.append(data + "\n");
             }
-        }).start();
+        });
     }
 
-    private void showNotification(Context context, String message) {
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    public void printServerLog(String data) {
+        Log.d("MainActivity", data);
 
-        NotificationChannel channel = new NotificationChannel("notification_channel", "알림", NotificationManager.IMPORTANCE_DEFAULT);
-        manager.createNotificationChannel(channel);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notification_channel")
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle("서버 응답")
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-        manager.notify(1, builder.build());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                serverText.append(data + "\n");
+            }
+        });
     }
+
+
+
 }
